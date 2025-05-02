f73ae5d87a268bc2a55c4f591695fc204f56382b
mirahashemi <mira.hashemi@ns.nl>
2025-05-02 19:13:23 +0200

    Refactoring: Clean up on Direction class - removal of unused methods

--- src/main/kotlin/Direction.kt
+++ src/main/kotlin/Direction.kt
@@ -17,41 +17,12 @@ enum class Direction(var shortName: Char) {
             }
         }
 
-        fun mapToChar(direction: Direction): Char {
-            return when (direction) {
-                North -> 'N'
-                West -> 'W'
-                South -> 'S'
-                East -> 'E'
-            }
-        }
-
-        fun whatIsOnTheLeftOf(direction: Char): Char { // Primitive Obsession
-            val mappedDirection = Direction.mapToDirection(direction)
-            return when (mappedDirection) {
-                East -> Direction.mapToChar(North)
-                North -> Direction.mapToChar(West)
-                West -> Direction.mapToChar(South)
-                South -> Direction.mapToChar(East)
-            }
-        }
-
-        fun whatIsOnTheLeftOf(direction: Direction): Direction { // Primitive Obsession
+        fun whatIsOnTheLeftOf(direction: Direction): Direction {
             return when (direction) {
                 East -> North
                 North -> West
                 West -> South
                 South -> East
-            }
-        }
-
-        fun whatIsOnTheRightOf(direction: Char): Char {
-            val mappedDirection = Direction.mapToDirection(direction)
-            return when (mappedDirection) {
-                East -> Direction.mapToChar(South)
-                South -> Direction.mapToChar(West)
-                West -> Direction.mapToChar(North)
-                North -> Direction.mapToChar(East)
             }
         }
 
